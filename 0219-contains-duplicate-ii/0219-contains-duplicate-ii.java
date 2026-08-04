32d2f3c4670953c6762ec12d5443032b44718754
BollimunthaAbhiram <vtu29535@veltech.edu.in>
2026-08-04 09:46:06 +0530

    Time: 28 ms (31.87%), Space: 111.1 MB (18.74%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
@@ -1,20 +1,16 @@
-import java.util.HashSet;
-import java.util.Set;
-
-public class Solution {
+class Solution {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
-        Set<Integer> window = new HashSet<>();
-        
+        HashMap<Integer, Integer> map = new HashMap<>();
+
         for (int i = 0; i < nums.length; i++) {
-            if (window.contains(nums[i])) {
-                return true;
+            if (map.containsKey(nums[i])) {
+                if (i - map.get(nums[i]) <= k) {
+                    return true;
+                }
             }
-            window.add(nums[i]);
-            if (window.size() > k) {
-                window.remove(nums[i - k]);
-            }
+            map.put(nums[i], i); // Update latest index
         }
-        
+
         return false;
     }
 }
